65a4c218525eca3bf8637f7ad274f86907bb2fed
Sandiletarget <Sandiletarget@gmail.com>
2026-01-16 16:42:13 +0200

    Fix DJANGO_SETTINGS_MODULE to edusa.settings (case-sensitive)

--- edusa/wsgi.py
+++ edusa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE=edusa.settings gunicorn edusa.wsgi:application')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusa.settings')
 
 application = get_wsgi_application()
